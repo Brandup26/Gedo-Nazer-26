ef1793d4560e0a68c0bf3cf8704a8ae7e09f15bf
Brandup26 <anasgaber504@gmail.com>
2026-06-23 01:23:16 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'jedo-nazir-v1';
+self.addEventListener('install', (e) => e.waitUntil(self.skipWaiting()));
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+    self.clients.claim();
+});
+self.addEventListener('fetch', (e) => {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
